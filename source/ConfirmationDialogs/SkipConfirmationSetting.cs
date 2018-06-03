d77bf4a1e128ed69713bb10c5936097a8cbe061c
Tobias Brohl <tobrohl@gmail.com>
2018-06-03 20:50:01 +0200

    Foreword

--- source/ConfirmationDialogs/SkipConfirmationSetting.cs
+++ source/ConfirmationDialogs/SkipConfirmationSetting.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 using System.Windows.Input;
 using System.Xml.Schema;
 
-namespace ConfirmationDialogs {
-	public struct SkipConfirmationSetting {
-		public bool AllowSkip;
-		public bool SkipAlways;
-		public ModifierRequirement Shift;
-		public ModifierRequirement Alt;
-		public ModifierRequirement Control;
-		public ModifierRequirement Windows;
+namespace ConfirmationDialogs
+{
+   public class SkipConfirmationSetting : ICloneable
+   {
+      public bool AllowSkip;
+      public bool SkipAlways;
+      public ModifierRequirement Shift;
+      public ModifierRequirement Alt;
+      public ModifierRequirement Control;
+      public ModifierRequirement Windows;
 
-		public static class Prests {
-			public static readonly SkipConfirmationSetting ShiftForSKip = new SkipConfirmationSetting {
-				Shift = ModifierRequirement.Required,
-				AllowSkip = true
-			};
+      public SkipConfirmationSetting()
+      {
 
-			public static readonly SkipConfirmationSetting SkipAlways = new SkipConfirmationSetting {SkipAlways = true};
-			public static readonly SkipConfirmationSetting NeverSkip= new SkipConfirmationSetting();
-		}
-		//public Dictionary<ModifierKeys,ModifierRequirement> Requirements;
+      }
+      public static class Prests
+      {
+         public static readonly SkipConfirmationSetting ShiftForSKip = new SkipConfirmationSetting
+         {
+            Shift = ModifierRequirement.Required,
+            AllowSkip = true
+         };
+
+         public static readonly SkipConfirmationSetting SkipAlways = new SkipConfirmationSetting { SkipAlways = true };
+         public static readonly SkipConfirmationSetting NeverSkip = new SkipConfirmationSetting();
+      }
+      //public Dictionary<ModifierKeys,ModifierRequirement> Requirements;
 
 
-	}
+      public object Clone()
+      {
+         return new SkipConfirmationSetting() { AllowSkip = AllowSkip, SkipAlways = SkipAlways, Shift = Shift, Alt = Alt, Control = Control, Windows = Windows };
+      }
+   }
 }

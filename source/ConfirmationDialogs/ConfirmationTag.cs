da13925dd9717357b8adbb1c14da798398323301
Tobias Brohl <tobrohl@gmail.com>
2018-06-06 08:45:22 +0200

    Bugs++

--- source/ConfirmationDialogs/ConfirmationTag.cs
+++ source/ConfirmationDialogs/ConfirmationTag.cs
@@ -3,13 +3,13 @@
 namespace ConfirmationDialogs {
 	internal class ConfirmationTag {
 		[NotNull]
-		public string Text { get; }
+		public string Text { get; set; }
       [NotNull]
-      public string Confirmation { get; }
+      public string Confirmation { get; set; }
       [NotNull]
-      public string ContinueButton { get; }
+      public string ContinueButton { get; set; }
       [NotNull]
-      public string CancelButton { get; }
+      public string CancelButton { get; set; }
       public bool Confirmed;
 		public ConfirmationTag(string text=null, string confirmation=null,string continueButton= null,string cancelButton=null)
 		{
